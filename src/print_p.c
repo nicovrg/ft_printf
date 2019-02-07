24a5cf04f4b5f30f0aae8a9bcd3fb43158dea00a
jquivogn <julesqvgn@mbp-de-jules.home>
2019-02-07 15:26:25 +0100

    passed all conversion just fix padding on unicode

--- src/print_p.c
+++ src/print_p.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   print_p.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jquivogn <jquivogn@student.42.fr>          +#+  +:+       +#+        */
+/*   By: julesqvgn <julesqvgn@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/17 23:29:11 by nivergne          #+#    #+#             */
-/*   Updated: 2019/02/04 15:57:25 by jquivogn         ###   ########.fr       */
+/*   Updated: 2019/02/07 15:22:47 by julesqvgn        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,7 @@ void	ft_addaddr(va_list ap, t_info *options)
 	void *cast;
 	char *tmp;
 
-	cast = (options->type == 0 ? ft_itoa_base(va_arg(ap, unsigned int), 16, 1) : 0);
-	cast = (options->type == 3 ? ft_itoa_base((unsigned char)(va_arg(ap, unsigned int)), 16, 1) : cast);	
-	cast = (options->type == 1 ? ft_itoa_base((unsigned short)(va_arg(ap, unsigned int)), 16, 1) : cast);
-	cast = (options->type == 2 ? ft_itoa_base(va_arg(ap, unsigned long), 16, 1) : cast);
-	cast = (options->type == 4 ? ft_itoa_base(va_arg(ap, unsigned long long), 16, 1) : cast);
+	cast = ft_itoa_base(va_arg(ap, unsigned long long), 16, 1);
 	tmp = ft_strjoin("0x", cast);
 	free(cast);
 	cast = tmp;
